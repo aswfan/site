92b27ecc162de85520f8a38eb504d4c540c7e072
fanyanf <fanyanf@amazon.com>
2018-08-12 13:17:34 -0700

    create repo

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 8000
+const address = process.env.ADDR || "http://localhost"
+
+app.get('/', (req, res) => res.send("Hello World!"));
+
+app.listen(8080, () => console.log(`listening to ${address}:${port}...`));
